926b485cf475d59f71636a1573a723672e6fdf58
mcotonea42 <mcotonea42@gmail.com>
2025-03-03 15:35:35 +0100

    Add ft_gettype function

--- mandatory/sources/parsing/tokenization.c
+++ mandatory/sources/parsing/tokenization.c
@@ -1,44 +1,53 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   token.c                                            :+:      :+:    :+:   */
+/*   tokenization.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmilliot <mmilliot@student.42.fr>          +#+  +:+       +#+        */
+/*   By: melvin <melvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 18:08:52 by mmilliot          #+#    #+#             */
-/*   Updated: 2025/02/28 18:49:45 by mmilliot         ###   ########.fr       */
+/*   Updated: 2025/03/03 15:34:31 by melvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-/* static int	ft_gettype(char *str)
-{
-	int		i;
-	char	*line;
 
-	while (*str)
+/* This function assigns a type (int) to each token in the list
+   Missing infiles, outfiles and differentiating arg from cmd */
+
+static void	ft_gettype(t_data *data)
+{
+	t_token *current;
+
+	current = data->lst_token;
+	while (current)
 	{
-		if (*str == '|')
-			return (PIPE);
-		if (*str == '<' || *str == '>')
-			return (REDIR);
-		if (*str == '<' && *str + 1 == '<')
-			return (HEREDOC);
-		if (*str == '>' && *str + 1 == '>')
-			return (APPEND);
-		if (*str == '&' && *str + 1 == '&')
-			return (AND);
-		if (*str == '|' && *str + 1 == '|')
-			return (OR);
+		if (ft_strcmp(current->line, "|") == 0)
+			current->token = PIPE;
+		else if ((ft_strcmp(current->line, "<") == 0) ||
+			(ft_strcmp(current->line, ">") == 0))
+			current->token = REDIR;
+		else if (ft_strcmp(current->line, "<<") == 0)
+			current->token = HEREDOC;
+		else if (ft_strcmp(current->line, ">>") == 0)
+			current->token = APPEND;
+		else if (ft_strcmp(current->line, "&&") == 0)
+			current->token = AND;
+		else if (ft_strcmp(current->line, "||") == 0)
+			current->token = OR;
+		else
+			current->token = CMD;
+		current = current->next;
 	}
-	return (0);
-} */
+	return ;
+}
 
 /* It is the main Function for the tokenisation !*/
 
 void	tokenization(t_data *data)
 {
 	cut_the_line(data);
+	ft_gettype(data);
 	return ;
 }
